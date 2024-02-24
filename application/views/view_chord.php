1871acfad16603329a1aaf5052daf66adcf343e9
minhngoc <minhngoc.ith@gmail.com>
2024-02-24 15:35:09 +0700

    15:34 24/02/2024: Remove jTab, using GuitarChord

--- application/views/view_chord.php
+++ application/views/view_chord.php
@@ -6,20 +6,44 @@
         <div class="chord__text">Vòng tròn hợp âm</div>
         <div class="chord__circle"><img src="<?php echo base_url("tmp/images/cf.svg"); ?>" alt=""></div>
         <div class="chord__list">
-          <ul>
-            <li class="active"><a data-chord="C">C</a></li>
-            <li><a data-chord="C#">C#</a></li>
-            <li><a data-chord="D">D</a></li>
-            <li><a data-chord="Eb">Eb</a></li>
-            <li><a data-chord="E">E</a></li>
-            <li><a data-chord="F">F</a></li>
-            <li><a data-chord="F#">F#</a></li>
-            <li><a data-chord="G">G</a></li>
-            <li><a data-chord="G#">G#</a></li>
-            <li><a data-chord="A">A</a></li>
-            <li><a data-chord="Bb">Bb</a></li>
-            <li><a data-chord="B">B</a></li>
-          </ul>
+        <select class="list-chord" name="chord">
+          <option value="C" selected>C</option>
+          <option value="C#">C#</option>
+          <option value="D">D</option>
+          <option value="Eb">Eb</option>
+          <option value="E">E</option>
+          <option value="F">F</option>
+          <option value="F#">F#</option>
+          <option value="G">G</option>
+          <option value="G#">G#</option>
+          <option value="A">A</option>
+          <option value="Bb">Bb</option>
+          <option value="B">B</option>
+        </select>
+        <select class="list-tail" name="tail">
+          <option value="" selected>major</option>
+          <option value="m">minor</option>
+          <option value="6">6</option>
+          <option value="m6">m6</option>
+          <option value="69">69</option>
+          <option value="7">7</option>
+          <option value="m7">m7</option>
+          <option value="maj7">maj7</option>
+          <option value="7#5">7#5</option>
+          <option value="m7b5">m7b5</option>
+          <option value="7b9">7b9</option>
+          <option value="7sus4">7sus4</option>
+          <option value="9">9</option>
+          <option value="m9">m9</option>
+          <option value="maj9">maj9</option>
+          <option value="add9">add9</option>
+          <option value="13">13</option>
+          <option value="sus2">sus2</option>
+          <option value="sus4">sus4</option>
+          <option value="dim">dim</option>
+          <option value="dim7">dim7</option>
+          <option value="aug">aug</option>
+        </select>
         </div>
         <div class="chord__result"></div>
       </div>
